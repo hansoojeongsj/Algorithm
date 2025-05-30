46eb3031e49c8054d950a2b8e126fb34da0f2254
hansoojeongsj <tnwjd3758@gmail.com>
2025-05-30 18:42:21 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 33832 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,28 @@
-# 유기농 배추 
-T = int(input()) #테스트케이스의 개수
+import sys
+sys.setrecursionlimit(10000)
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-def bfs(x,y):           
-    queue = [(x,y)]
-    matrix[x][y] = 0 # 방문처리
-
-    while queue:
-        x,y = queue.pop(0)
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
-                continue
-
-            if matrix[nx][ny] == 1 :
-                queue.append((nx,ny))
-                matrix[nx][ny] = 0
-
-# 행렬만들기
-for i in range(T):
-    M, N, K = map(int,input().split())
-    matrix = [[0]*(N) for _ in range(M)]
-    cnt = 0
-
-    for j in range(K):
-        x,y = map(int, input().split())
-        matrix[x][y] = 1
-
-    for a in range(M):
-        for b in range(N):
-            if matrix[a][b] == 1:
-                bfs(a,b)
-                cnt += 1
-
-    print(cnt)
+def dfs(x, y):
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (0 <= nx < m) and (0 <= ny < n):
+            if field[ny][nx] == 1:
+                field[ny][nx] = 0
+                dfs(nx, ny)
+                
+for _ in range(int(sys.stdin.readline())):
+    m, n, k = map(int, sys.stdin.readline().split())
+    field = [[0 for _ in range(m)] for _ in range(n)]
+    count = 0
+    for _ in range(k):
+        x, y = map(int, sys.stdin.readline().split())
+        field[y][x] = 1
+    for x in range(m):
+        for y in range(n):
+            if field[y][x] == 1:
+                dfs(x, y)
+                count += 1
+    print(count)
